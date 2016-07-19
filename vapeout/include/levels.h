132848e19d2a846e4d7dafb8c6d433d6cf615885
Chris Roy <christofer.roy@gmail.com>
2016-07-19 01:27:43 -0400

    working on levels

--- vapeout/include/levels.h
+++ vapeout/include/levels.h
@@ -3,5 +3,15 @@
 
 #include <stdlib.h>
 
+struct levelDesc {
+    uint8_t width;
+    uint8_t height;
+    uint8_t layout[];
+};
 
+struct levelDesc level1Desc = {
+    .width = 10,
+    .height = 2,
+    .layout = {1,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1}
+};
 #endif // VAPEOUT_LEVELS_H
